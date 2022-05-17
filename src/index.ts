db7091f5009a29ef5d40eb2451305e0118adb2d9
Jerrell Jones <jayjonesdevelopment@gmail.com>
2022-05-17 09:13:25 -0400

    Use env var for socket-io CORS origin

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,9 @@ import { Clients } from './types/event.types';
 const app: Application = express();
 const PORT = process.env.PORT || 5555;
 const httpServer = createServer(app);
-
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST'],
   },
 });
