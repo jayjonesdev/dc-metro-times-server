2c39f6962ad08bd4c411ab5a501ce84515b047f1
Jerrell Jones <jayjonesdevelopment@gmail.com>
2022-09-10 16:50:22 -0400

    feat: create docker file and docker commands

--- src/controllers/rail.controller.ts
+++ src/controllers/rail.controller.ts
@@ -1,16 +1,19 @@
-import { Router } from "express";
-import { fetchRailIncidents, fetchRealTimeRailPredictions } from "../actions/rail";
+import { Router } from 'express';
+import {
+  fetchRailIncidents,
+  fetchRealTimeRailPredictions,
+} from '../actions/rail';
 
 const railRouter = Router();
 
 railRouter.get('/incidents', async (_req, res) => {
-    const incidents = await fetchRailIncidents();
-    res.send(incidents);
+  const incidents = await fetchRailIncidents();
+  res.send(incidents);
 });
 
 railRouter.get('/realtime', async (_req, res) => {
-    const realtimePredictions = await fetchRealTimeRailPredictions();
-    res.send(realtimePredictions);
+  const realtimePredictions = await fetchRealTimeRailPredictions();
+  res.send(realtimePredictions);
 });
 
 export default railRouter;
